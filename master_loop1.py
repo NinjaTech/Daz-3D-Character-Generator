edfc78777c40de01acb346e2444c43d32b6ed7e9
NinjaTech <charles@galaxytron.com>
2023-04-11 03:39:39 -0500

    Add files via upload

---
+++ master_loop1.py
@@ -0,0 +1,12 @@
+import subprocess
+import os
+import pwd
+import grp
+import shutil
+import time
+
+while True:
+      os.system('python /home/randy/Dropbox/deca/deep3d/img_proc1.py')
+      time.sleep(90)
+      os.system('python /home/randy/Dropbox/deca/deep3d/master4.py')
+      time.sleep(90)
